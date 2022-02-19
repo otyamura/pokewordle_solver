2e7473b7ea09ebcbd3703317661e6827944cc82c
otyamura <suzuki08suzuki@gmail.com>
2022-02-19 18:20:35 +0900

    fix route

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -21,7 +21,7 @@ func CreateConnection() (*gorm.DB, *gin.Engine) {
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello!",
 		})
