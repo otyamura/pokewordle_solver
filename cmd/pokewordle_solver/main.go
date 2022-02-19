4a5d41cbe092fb567a655cac32567d4fc08ad2d4
otyamura <suzuki08suzuki@gmail.com>
2022-02-19 18:27:00 +0900

    fix port

--- cmd/pokewordle_solver/main.go
+++ cmd/pokewordle_solver/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
 	con "github.com/otyamura/pokewordle_solver/internal/connection"
 )
 
 func main() {
 	_, r := con.CreateConnection()
-	err := r.Run()
+	p := os.Getenv("PORT")
+	if p != "" {
+		p = ":" + p
+	} else {
+		p = ":8080"
+	}
+	err := r.Run(p)
 	if err != nil {
 		log.Fatal(err)
 	}
